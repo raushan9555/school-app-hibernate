170a59c2145f6c46251ee3498481fa1cdff76dd6
Raushan Saxena <raushan.javadeveloper@gmail.com>
2022-09-27 07:25:15 +0530

    dto

---
+++ Student.java
@@ -0,0 +1,31 @@
+package com.ty.school.dto;
+	import javax.persistence.Entity;
+	import javax.persistence.Id;
+
+	@Entity
+	public class Student {
+		@Id
+		private int rol;
+		private String name;
+		private double percentage;
+		public int getRol() {
+			return rol;
+		}
+		public void setRol(int rol) {
+			this.rol = rol;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public double getPercentage() {
+			return percentage;
+		}
+		public void setPercentage(double percentage) {
+			this.percentage = percentage;
+		}
+
+	}
+
---
+++ User.java
@@ -0,0 +1,51 @@
+package com.ty.school.dto;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+	@Entity
+	public class User {
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		private int id;
+		@Column(name="my_email",unique=true)
+		private String email;
+		@Column(name="user_password",nullable=false)
+		private String password;
+		private long phone;
+		private String gender;
+		public int getId() {
+			return id;
+		}
+		public void setId(int id) {
+			this.id = id;
+		}
+		public String getEmail() {
+			return email;
+		}
+		public void setEmail(String email) {
+			this.email = email;
+		}
+		public String getPassword() {
+			return password;
+		}
+		public void setPassword(String password) {
+			this.password = password;
+		}
+		public long getPhone() {
+			return phone;
+		}
+		public void setPhone(long phone) {
+			this.phone = phone;
+		}
+		public String getGender() {
+			return gender;
+		}
+		public void setGender(String gender) {
+			this.gender = gender;
+		}
+
+
+}
